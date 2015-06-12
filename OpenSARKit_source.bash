14ab02853c0e894a0f563fff9b66d4929a6d6d8b
BuddyVolly <andreasvollrath@gmx.de>
2015-06-12 11:44:09 +0200

    add working directories and source file

---
+++ OpenSARKit_source.bash
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Support script to source the original programs
+
+# Folder of OpenSARKit scripts and workflows
+export OPENSARKIT="~/github/OpenSARKit"
+
+# source worklows/graphs
+export NEST_GRAPHS="${OPENSARKIT}/graphs/NEST"
+export S1TBX_GRAPHS="${OPENSARKIT}/graphs/S1TBX"
+
+# Folder of Program installations
+PROGRAMS="/usr/programs/"
+export NEST_BIN="${PROGRMAS}/NEST"
+export S1TBX_BIN="${PROGRAMS}/S1TBX"
+
+# ASF Mapready
+# GMTSAR
+# DORIS 
+
+
