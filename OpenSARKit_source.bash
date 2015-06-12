ebde61fcc12722a97b6ee3663b229a217985ce4f
BuddyVolly <andreasvollrath@gmx.de>
2015-06-12 13:17:58 +0200

    extent source files with aliases

--- OpenSARKit_source.bash
+++ OpenSARKit_source.bash
@@ -2,6 +2,15 @@
 
 # Support script to source the original programs
 
+# Folder of external program installations
+PROGRAMS="/usr/programs"
+export NEST_EXE=${PROGRAMS}/NEST/gpt.sh
+export S1TBX_EXE=${PROGRAMS}/S1TBX/gpt.sh
+# ASF Mapready
+# GMTSAR
+# DORIS 
+
+
 # Folder of OpenSARKit scripts and workflows
 export OPENSARKIT="~/github/OpenSARKit"
 
@@ -9,13 +18,10 @@ export OPENSARKIT="~/github/OpenSARKit"
 export NEST_GRAPHS="${OPENSARKIT}/graphs/NEST"
 export S1TBX_GRAPHS="${OPENSARKIT}/graphs/S1TBX"
 
-# Folder of Program installations
-PROGRAMS="/usr/programs/"
-export NEST_BIN="${PROGRMAS}/NEST"
-export S1TBX_BIN="${PROGRAMS}/S1TBX"
-
-# ASF Mapready
-# GMTSAR
-# DORIS 
+# source worklows/graphs
+export NEST_BIN="${OPENSARKIT}/bins/NEST"
+export S1TBX_BIN="${OPENSARKIT}/bins/S1TBX"
 
 
+# Aliases
+alias osk_ALOS_CEOS_import="bash ${NEST_BIN}/osk_import_ALOS_L1_1_CEOS_to_dim.sh"
