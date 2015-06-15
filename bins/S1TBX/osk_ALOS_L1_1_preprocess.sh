6b1b5ef637df4cb75a9a5f2dd48d36a923ce75c9
BuddyVolly <andreasvollrath@gmx.de>
2015-06-15 16:33:46 +0200

    adding texture and ratio to alos_preprocess

--- bins/S1TBX/osk_ALOS_L1_1_preprocess.sh
+++ bins/S1TBX/osk_ALOS_L1_1_preprocess.sh
@@ -25,8 +25,8 @@ if [ "$#" != "1" ]; then
   echo -e "The path will be your Project folder!"
   exit 1
 else
+  echo "Welcome to OpenSARKit!"
   echo "Processing folder: 	$1"
-  echo "OpenSARKit"
 fi
 
 #	0.2 Define Workspace
@@ -36,19 +36,21 @@ export ZIP_DIR="${PROC_DIR}/ZIP"
 export INPUT_DIR="${PROC_DIR}/DIM_INPUT" # Imported DIMAP raw data
 export ML_SPK_DIR="${PROC_DIR}/ML_SPK"
 export SPK_DIV_DIR="${PROC_DIR}/SPK_DIV"
+export RATIO_DIR="${PROC_DIR}/RATIO"
 export TEXTURE_DIR="${PROC_DIR}/TEXTURE"
 export POLSAR_DIR="${PROC_DIR}/POLSAR"
-export FINAL_DIR="${PROC_DIR}/FINAL"
+export GAMMA_DIR="${PROC_DIR}/FINAL"
 
 #	0.3 Create Workspace
 mkdir -p ${ZIP_DIR}
 mkdir -p ${TMP_DIR}
 mkdir -p ${INPUT_DIR}
-mkdir -p ${ML_SPK_DIR}
-mkdir -p ${SPK_DIV_DIR}
-mkdir -p ${TEXTURE_DIR}
-mkdir -p ${POLSAR_DIR}
-mkdir -p ${FINAL_DIR}
+#mkdir -p ${ML_SPK_DIR}
+#mkdir -p ${SPK_DIV_DIR}
+#mkdir -p ${TEXTURE_DIR}
+#mkdir -p ${POLSAR_DIR}
+#mkdir -p ${RATIO_DIR}
+#mkdir -p ${GAMMA_DIR}
 
 #	0.4 DEM File for Geocoding
 DEM_FILE='/home/avollrath/test/final_dem_filled.tif'
@@ -73,18 +75,47 @@ for FILE in `ls -1 ${ZIP_DIR}`;do
 	cd ${TMP_DIR}/${SCENE_ID}
 	VOLUME_FILE=`ls VOL*`
 
+	# check for mode
+	if grep -q IMG-VV workreport;then
+
+		MODE="PLR"
+
+	elif grep -q IMG-HV workreport;then
+
+		MODE="FBD"
+	
+	else
+
+		MODE="FBS"
+	fi
+
 	# extract Date and Footprint
 	YEAR=`cat workreport | grep Img_SceneCenterDateTime | awk -F "=" $'{print $2}' | cut -c 2-5`
+	MONTH=`cat workreport | grep Img_SceneCenterDateTime | awk -F "=" $'{print $2}' | cut -c 6-7`
+	DAY=`cat workreport | grep Img_SceneCenterDateTime | awk -F "=" $'{print $2}' | cut -c 7-8`
 	DATE=`cat workreport | grep Img_SceneCenterDateTime | awk -F "=" $'{print $2}' | cut -c 2-9`
-	UL_LAT=`cat workreport | grep Brs_ImageSceneLeftTopLatitude | awk -F "=" $'{print $2}' | sed 's/\"//g'`
+#	UL_LAT=`cat workreport | grep Brs_ImageSceneLeftTopLatitude | awk -F "=" $'{print $2}' | sed 's/\"//g'`
 #	UL_LAT=`cat workreport | grep Brs_ImageSceneLeftTopLatitude | awk -F "=" $'{print $2}' | sed 's/\"//g'`
 	
 	FRAME=`echo ${SCENE_ID}	| cut -c 12-15`	
 
 	# !!!!!needs change for final version!!!!!	
-	#SAT_PATH=`curl http://api.daac.asf.alaska.edu/services/search/param?keyword=value\&granule_list=${SCENE_ID}\&output=csv | tail -n 1 | awk -F "," $'{print $7}' | sed 's/\"//g'`
+	SAT_PATH=`curl -s http://api.daac.asf.alaska.edu/services/search/param?keyword=value\&granule_list=${SCENE_ID:0:15}\&output=csv | tail -n 1 | awk -F "," $'{print $7}' | sed 's/\"//g'`
 	
-	#echo "$FRAME $PATH $YEAR $UL_LAT"
+	echo "Processing Scene: ${SCENE_ID:0:15}"
+	echo "Satellite/Sensor: ALOS/Palsar"
+	echo "Acquisiton Mode: ${MODE}"
+	echo "Acquisition Date (YYYYMMDD): ${DATE}"
+	echo "Relative Satellite Track: ${SAT_PATH}"
+	echo "Image Frame: $FRAME"
+
+	mkdir -p ${PROC_DIR}/${MODE}
+	mkdir -p ${PROC_DIR}/${MODE}/${YEAR}
+	mkdir -p ${PROC_DIR}/${MODE}/${YEAR}/${SAT_PATH}
+	mkdir -p ${PROC_DIR}/${MODE}/${YEAR}/${SAT_PATH}/${FRAME}
+	mkdir -p ${PROC_DIR}/${MODE}/${YEAR}/${SAT_PATH}/${FRAME}/${DATE}
+
+	FINAL_DIR=${PROC_DIR}/${MODE}/${YEAR}/${SAT_PATH}/${FRAME}/${DATE}
 
 #----------------------------------------------------------------------
 # 	1 Import Raw data to DIMAP format compatible with S1TBX
@@ -92,95 +123,71 @@ for FILE in `ls -1 ${ZIP_DIR}`;do
 
 	# define input/output
 	INPUT_RAW=${TMP_DIR}/${SCENE_ID}/${VOLUME_FILE}	
-	OUTPUT_DIMAP=${INPUT_DIR}/${SCENE_ID}.dim
+	OUTPUT_DIMAP=${FINAL_DIR}/${SCENE_ID}.dim
 
 	# Write new xml graph and substitute input and output files
 	cp ${NEST_GRAPHS}/ALOS_L1.1_NEST_import.xml ${TMP_DIR}/Import_DIMAP.xml
 	
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_RAW</file>|<file>${INPUT_RAW}</file>|g" ${TMP_DIR}/Import_DIMAP.xml
+	sed -i "s|INPUT_RAW|${INPUT_RAW}|g" ${TMP_DIR}/Import_DIMAP.xml
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_DIMAP</file>|<file>${OUTPUT_DIMAP}</file>|g" ${TMP_DIR}/Import_DIMAP.xml
+	sed -i "s|OUTPUT_DIMAP|${OUTPUT_DIMAP}|g" ${TMP_DIR}/Import_DIMAP.xml
 
-	echo "Importing CEOS files to BEAM_DIMAP file format for ${SCENE_ID}"
+	echo "Importing CEOS files to BEAM_DIMAP file format for ${SCENE_ID} from $DATE"
 	sh ${NEST_EXE} ${TMP_DIR}/Import_DIMAP.xml
+
 
 #----------------------------------------------------------------------
 # 	2 Create multilooked, Lee speckle-filtered intensities
 #----------------------------------------------------------------------	
 
+	
 	# define output
-	OUTPUT_ML_SPK=${ML_SPK_DIR}/${SCENE_ID}"_ML_SPK.dim"
+	OUTPUT_ML_SPK=${TMP_DIR}/${SCENE_ID}"_ML_SPK.dim"
 	# Write new xml graph and substitute input and output files
 	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_DSK_ML_SPK.xml ${TMP_DIR}/ML_SPK.xml
 
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_DIMAP</file>|<file>${OUTPUT_DIMAP}</file>|g" ${TMP_DIR}/ML_SPK.xml
+	sed -i "s|INPUT_DIMAP|${OUTPUT_DIMAP}|g" ${TMP_DIR}/ML_SPK.xml
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_ML_SPK</file>|<file>${OUTPUT_ML_SPK}</file>|g" ${TMP_DIR}/ML_SPK.xml
+	sed -i "s|OUTPUT_ML_SPK|${OUTPUT_ML_SPK}|g" ${TMP_DIR}/ML_SPK.xml
 
 	echo "Apply Multi-look & Speckle Filter to ${SCENE_ID}"
 	sh ${S1TBX_EXE} ${TMP_DIR}/ML_SPK.xml
 
-			
+	# Geocode Multi-looked, speckle-filtered imagery
+
+	OUTPUT_ML_SPK_TR=${FINAL_DIR}/${SCENE_ID}'_ML_SPK_TR.dim'
+	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_TR_radiometric.xml ${TMP_DIR}/TR_ML_SPK.xml
+
+	# insert Input file path into processing chain xml
+	sed -i "s|INPUT_TR|${OUTPUT_ML_SPK}|g" ${TMP_DIR}/TR_ML_SPK.xml
+	# insert Input file path into processing chain xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_ML_SPK_TR}|g" ${TMP_DIR}/TR_ML_SPK.xml
+	# insert DEM path
+	sed -i "s|DEM_FILE|${DEM_FILE}|g" ${TMP_DIR}/TR_ML_SPK.xml
+
+	# Radiometrically terrain correcting Multi-looked, speckle-filtered files
+	echo "Geocode Multi-looked, speckle-filtered scene: ${SCENE_ID}"
+	sh ${S1TBX_EXE} ${TMP_DIR}/TR_ML_SPK.xml
+
+		
 #----------------------------------------------------------------------
 # 	3 Create Speckle Divergence files
 #----------------------------------------------------------------------	
 
 	# define path/name of output
-	OUTPUT_SPK_DIV=${SPK_DIV_DIR}/${SCENE_ID}"_SPK_DIV.dim"
+	OUTPUT_SPK_DIV=${TMP_DIR}/${SCENE_ID}"_SPK_DIV.dim"
 	# Write new xml graph and substitute input and output files
 	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_DSK_SPK-DIV_ML.xml ${TMP_DIR}/SPK_DIV.xml
 
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_DIMAP</file>|<file>${OUTPUT_DIMAP}</file>|g" ${TMP_DIR}/SPK_DIV.xml
+	sed -i "s|INPUT_DIMAP|${OUTPUT_DIMAP}|g" ${TMP_DIR}/SPK_DIV.xml
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_SPK_DIV</file>|<file>${OUTPUT_SPK_DIV}</file>|g" ${TMP_DIR}/SPK_DIV.xml
+	sed -i "s|OUTPUT_SPK_DIV|${OUTPUT_SPK_DIV}|g" ${TMP_DIR}/SPK_DIV.xml
 
 	echo "Calculate Speckle Divergence and apply Multi-looking for ${SCENE_ID}"
 	sh ${S1TBX_EXE} ${TMP_DIR}/SPK_DIV.xml
-
-	
-#----------------------------------------------------------------------
-# 	4 Create Polarimetric Products
-#----------------------------------------------------------------------	
-
-	# define path/name of output
-	OUTPUT_POLSAR=${POLSAR_DIR}/${SCENE_ID}"_H_alpha.dim"
-	# Write new xml graph and substitute input and output files
-	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_H_alpha.xml ${TMP_DIR}/POLSAR.xml
-	
-	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_DIMAP</file>|<file>${OUTPUT_DIMAP}</file>|g" ${TMP_DIR}/POLSAR.xml
-	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_POLSAR</file>|<file>${OUTPUT_POLSAR}</file>|g" ${TMP_DIR}/POLSAR.xml
-
-	echo "Calculate H-alpha dual pol decomposition for ${SCENE_ID}"
-	sh ${S1TBX_EXE} ${TMP_DIR}/POLSAR.xml
-
-
-#----------------------------------------------------------------------
-# 	5 Geocode Products
-#----------------------------------------------------------------------	
-
-
-	# 5a	Geocode Multi-looked, speckle-filtered imagery
-
-	OUTPUT_ML_SPK_TR=${FINAL_DIR}/${SCENE_ID}'_ML_SPK_TR.tif'
-	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_TR_radiometric.xml ${TMP_DIR}/TR_ML_SPK.xml
-
-	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_TR</file>|<file>${OUTPUT_SPK_DIV}</file>|g" >> ${TMP_DIR}/TR_ML_SPK.xml
-	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_TR</file>|<file>${OUTPUT_SPK_DIV_TR}</file>|g" >> ${TMP_DIR}/TR_ML_SPK.xml
-	# insert DEM path
-	sed -i "s|<externalDEMFile>DEM_FILE</externalDEMFile>|<externalDEMFile>${DEM_FILE}</externalDEMFile>|g" >> ${TMP_DIR}/TR_ML_SPK.xml
-
-	# Radiometrically terrain correcting Multi-looked, speckle-filtered files
-	echo "Geocode Multi-looked, speckle-filtered scene: ${SCENE_ID}"
-	sh ${S1TBX_EXE} ${TMP_DIR}/TR_ML_SPK.xml
-	
-
 
 	# 5b	Geocode Speckle-Divergence
 
@@ -190,16 +197,32 @@ for FILE in `ls -1 ${ZIP_DIR}`;do
 	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_TR_SPK_DIV.xml ${TMP_DIR}/TR_SPK_DIV.xml
 
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_TR</file>|<file>${OUTPUT_SPK_DIV}</file>|g" >> ${TMP_DIR}/TR_SPK_DIV.xml
+	sed -i "s|INPUT_TR|${OUTPUT_SPK_DIV}|g" ${TMP_DIR}/TR_SPK_DIV.xml
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_TR</file>|<file>${OUTPUT_SPK_DIV_TR}</file>|g" >> ${TMP_DIR}/TR_SPK_DIV.xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_SPK_DIV_TR}|g" ${TMP_DIR}/TR_SPK_DIV.xml
 	# insert external DEM path
-	sed -i "s|<externalDEMFile>DEM_FILE</externalDEMFile>|<externalDEMFile>${DEM_FILE}</externalDEMFile>|g" >> ${TMP_DIR}/TR_SPK_DIV.xml
+	sed -i "s|DEM_FILE|${DEM_FILE}|g" ${TMP_DIR}/TR_SPK_DIV.xml
 
 	# Radiometrically terrain correcting Multi-looked, speckle-filtered files
 	echo "Geocode Speckle-Divergence from scene: ${SCENE_ID}"
 	sh ${S1TBX_EXE} ${TMP_DIR}/TR_SPK_DIV.xml
+
+#----------------------------------------------------------------------
+# 	4 Create Polarimetric Products
+#----------------------------------------------------------------------	
+
+	# define path/name of output
+	OUTPUT_POLSAR=${TMP_DIR}/${SCENE_ID}"_H_alpha.dim"
+	# Write new xml graph and substitute input and output files
+	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_H_alpha.xml ${TMP_DIR}/POLSAR.xml
 	
+	# insert Input file path into processing chain xml
+	sed -i "s|INPUT_DIMAP|${OUTPUT_DIMAP}|g" ${TMP_DIR}/POLSAR.xml
+	# insert Input file path into processing chain xml
+	sed -i "s|OUTPUT_POLSAR|${OUTPUT_POLSAR}|g" ${TMP_DIR}/POLSAR.xml
+
+	echo "Calculate H-alpha dual pol decomposition for ${SCENE_ID}"
+	sh ${S1TBX_EXE} ${TMP_DIR}/POLSAR.xml
 
 	# 5c	Multi-look & Geocode Polsar H-alpha dual pol data (multilook included, since it does not work for the preproc chain)
 
@@ -209,18 +232,70 @@ for FILE in `ls -1 ${ZIP_DIR}`;do
 	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_TR_ML_H_alpha.xml ${TMP_DIR}/TR_H_alpha.xml
 
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>INPUT_TR</file>|<file>${OUTPUT_POLSAR}</file>|g" >> ${TMP_DIR}/TR_H_alpha.xml
+	sed -i "s|INPUT_TR|${OUTPUT_POLSAR}|g" ${TMP_DIR}/TR_H_alpha.xml
 	# insert Input file path into processing chain xml
-	sed -i "s|<file>OUTPUT_TR</file>|<file>${OUTPUT_POLSAR_TR}</file>|g" >> ${TMP_DIR}/TR_H_alpha.xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_POLSAR_TR}|g" ${TMP_DIR}/TR_H_alpha.xml
 	# insert external DEM path
-	sed -i "s|<externalDEMFile>DEM_FILE</externalDEMFile>|<externalDEMFile>${DEM_FILE}</externalDEMFile>|g" >> ${TMP_DIR}/TR_H_alpha.xml
+	sed -i "s|DEM_FILE|${DEM_FILE}|g" ${TMP_DIR}/TR_H_alpha.xml
 
 	# Radiometrically terrain correcting Multi-looked, speckle-filtered files
 	echo "Geocode Speckle-Divergence from scene: ${SCENE_ID}"
 	sh ${S1TBX_EXE} ${TMP_DIR}/TR_H_alpha.xml
 
+
 #----------------------------------------------------------------------
-# 	5 Remove tmp files
+# 	5 HH/HV ratio
+#----------------------------------------------------------------------	
+
+	# define path/name of output
+	OUTPUT_RATIO=${FINAL_DIR}/${SCENE_ID}"_HVHH_ratio.dim"
+	# Write new xml graph and substitute input and output files
+	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_HVHH_ratio.xml ${TMP_DIR}/RATIO.xml
+	
+	# insert Input file path into processing chain xml
+	sed -i "s|INPUT_TR|${OUTPUT_ML_SPK_TR}|g" ${TMP_DIR}/RATIO.xml
+	# insert Input file path into processing chain xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_RATIO}|g" ${TMP_DIR}/RATIO.xml
+
+	echo "Calculating HV/HH ratio ${SCENE_ID}"
+	sh ${S1TBX_EXE} ${TMP_DIR}/RATIO.xml
+
+#----------------------------------------------------------------------
+# 	6 Texture
+#----------------------------------------------------------------------	
+
+	# HH texture calculations
+
+	# define path/name of output
+	OUTPUT_TEXTURE_HH=${FINAL_DIR}/${SCENE_ID}"_TEXTURE_HH.dim"
+	# Write new xml graph and substitute input and output files
+	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_Texture_HH.xml ${TMP_DIR}/TEXTURE_HH.xml
+	
+	# insert Input file path into processing chain xml
+	sed -i "s|INPUT_TR|${OUTPUT_ML_SPK_TR}|g" ${TMP_DIR}/TEXTURE_HH.xml
+	# insert Input file path into processing chain xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_TEXTURE_HH}|g" ${TMP_DIR}/TEXTURE_HH.xml
+
+	echo "Calculate GLCM Texture measurements for HH channel"
+	sh ${S1TBX_EXE} ${TMP_DIR}/TEXTURE_HH.xml
+
+	# HV texture calculations
+
+	# define path/name of output
+	OUTPUT_TEXTURE_HV=${FINAL_DIR}/${SCENE_ID}"_TEXTURE_HV.dim"
+	# Write new xml graph and substitute input and output files
+	cp ${S1TBX_GRAPHS}/ALOS_FBD_1_1_Texture_HV.xml ${TMP_DIR}/TEXTURE_HV.xml
+	
+	# insert Input file path into processing chain xml
+	sed -i "s|INPUT_TR|${OUTPUT_ML_SPK_TR}|g" ${TMP_DIR}/TEXTURE_HV.xml
+	# insert Input file path into processing chain xml
+	sed -i "s|OUTPUT_TR|${OUTPUT_TEXTURE_HV}|g" ${TMP_DIR}/TEXTURE_HV.xml
+
+	echo "Calculate GLCM Texture measurements for HV channel"
+	sh ${S1TBX_EXE} ${TMP_DIR}/TEXTURE_HV.xml
+
+#----------------------------------------------------------------------
+# 	7 Remove tmp files
 #----------------------------------------------------------------------	
 
 	rm -rf ${TMP_DIR}/*
