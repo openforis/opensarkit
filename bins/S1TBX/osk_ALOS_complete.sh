f870574b0c55a66e2a2ef11e0983dba200f7fb09
BuddyVolly <andreasvollrath@gmx.de>
2015-06-26 17:09:33 +0200

    add full processing script

--- bins/S1TBX/osk_ALOS_complete.sh
+++ bins/S1TBX/osk_ALOS_complete.sh
@@ -7,7 +7,7 @@ cd $WORKSPACE
 
 for JAHR in `ls -1`; do
 
-	osk_bulk_ALOS_L1_1_preprocess $WORKSPACE/$JAHR /data/home/Andreas.Vollrath/datasets/Sri_Lanka/DEM/Sri_Lanka_SRTM1_filled.tif
+	osk_bulk_ALOS_FBD_L1.1_preprocess $WORKSPACE/$JAHR /data/home/Andreas.Vollrath/datasets/Sri_Lanka/DEM/Sri_Lanka_SRTM1_filled.tif
 
 	osk_ALOS_merge_path $WORKSPACE/$JAHR
 
