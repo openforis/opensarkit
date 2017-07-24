789aada4e5497352bb36b07f2326787ec919e527
BuddyVolly <andreasvollrath@gmx.de>
2017-07-24 13:57:04 +0200

    change installation procedure

---
+++ bins/Install_OST/ost_dependencies_ubuntu1604.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+# Copyright (c) 2016 Andreas Vollrath
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is furnished
+# to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+
+#----------------------------------
+# 1 Adding extra repositories
+#----------------------------------
+
+RELEASE=`lsb_release -sc`
+
+SECONDS=0
+echo -ne " Adding the Ubuntu GIS unstable repository ..." &&
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable > ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+
+SECONDS=0
+echo -ne " Adding the multiverse repository ..." &&
+add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ ${RELEASE} main multiverse"  >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+## IX R-CRAN from R mirror
+if grep -q "qgis.org/ubuntugis" /etc/apt/sources.list;then
+
+	echo "detected cran repository for R installation"
+else
+	add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu ${RELEASE}/"
+	gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9 >> ${OSK_HOME}/LOG/log_install 2>&1
+	gpg -a --export E084DAB9 | apt-key add - >> ${OSK_HOME}/LOG/log_install 2>&1
+fi
+
+if grep -q "qgis.org/ubuntugis" /etc/apt/sources.list;then
+	echo " Yeah, you are already a QGIS user, nice!"
+else
+
+	echo " "
+	echo " Adding the QGIS repository"
+	echo " Note: QGIS will not be installed, in order to do so type: "
+	echo " sudo apt-get install --yes qgis libqgis-dev"
+	echo " "
+	echo "deb http://qgis.org/ubuntugis ${RELEASE} main" >> /etc/apt/sources.list
+	echo "deb-src http://qgis.org/ubuntugis ${RELEASE} main" >> /etc/apt/sources.list
+	# add key
+	#apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811 >> ${OSK_HOME}/LOG/log_install 2>&1
+	#apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
+
+	wget -O - http://qgis.org/downloads/qgis-2016.gpg.key | gpg --import >> ${OSK_HOME}/LOG/log_install 2>&1
+	gpg --export --armor 073D307A618E5811 | apt-key add - >> ${OSK_HOME}/LOG/log_install 2>&1
+fi
+
+#------------------------------------------------------------------
+# 2 run update to load new packages and upgrade all installed ones
+#------------------------------------------------------------------
+
+SECONDS=0
+echo -ne " Updating the system ..."
+apt-get update -y >> ${OSK_HOME}/LOG/log_install 2>&1
+apt-get upgrade -y >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+#------------------------------------------------------------------
+# 3 install packages
+#------------------------------------------------------------------
+
+SECONDS=0
+echo -ne " Installing GIS/Remote sensing packages ..."
+apt-get install --yes --allow-unauthenticated \
+											gdal-bin \
+											libgdal-dev \
+ 										  dans-gdal-scripts \
+											saga \
+											libsaga-dev \
+										#	otb-bin \
+										# libotb \
+ 										#	libotb-apps \
+											geotiff-bin \
+											libgeotiff-dev \
+											spatialite-bin \
+											spatialite-gui >> ${OSK_HOME}/LOG/log_install_gis 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+
+SECONDS=0
+echo -ne " Installing Ubuntu package dependencies "
+apt-get install --yes --allow-unauthenticated \
+											mlocate \
+											libcunit1-dev \
+											libfftw3-dev \
+											libshp-dev \
+											libtiff5-dev \
+											libproj-dev \
+											flex \
+											bison \
+											libgsl0-dev \
+											gsl-bin \
+											libglade2-dev \
+											libgmp-dev \
+											libgtk2.0-dev \
+											pkg-config \
+											aria2 \
+											curl \
+											unrar \
+											p7zip-full \
+											parallel \
+											xml-twig-tools \
+											git \
+											libudunits2-dev \
+											libxinerama-dev \
+											libxrandr-dev \
+											libxcursor-dev \
+											swig \
+											r-base \
+											libv8-3.14-dev >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+SECONDS=0
+echo -ne " Installing python 2.7 packages "
+apt-get install --yes --allow-unauthenticated \
+											python-dev \
+											python-gdal \
+											python-saga \
+										#	python-otb \
+											python-scipy \
+											python-h5py \
+											python-skimage \
+											python-statsmodels \
+											python-pandas \
+											python-geopandas \
+											python-geopy \
+											python-progressbar \
+											python-opencv \
+											python-numpy >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+echo ""
+echo "-------------------------------------"
+echo "--- Installing Orfeo ToolBox ---"
+echo "-------------------------------------"
+
+# install the latest Orfeo packaged version for use of ORFEO remote modules
+otb=OTB-contrib-6.0.0-Linux64
+wget https://www.orfeo-toolbox.org/packages/$otb.run
+chmod +x $otb.run
+mv $otb.run /usr/local/lib
+cd /usr/local/lib
+./$otb.run
+rm $otb.run
+ln -s $otb orfeo
+chmod o+rx orfeo/*.sh
+chmod o+rx orfeo/otbenv.profile
+cd -
+echo "PYTHONPATH=/usr/local/lib/OTB-contrib-6.0.0-Linux64/lib/python" >> /etc/environment
+echo "PATH=${PATH}:/usr/local/lib/orfeo/bin" >> /etc/environment
+
+updatedb
+
+SECONDS=0
+echo -ne " Downloading the SNAP software ..." &&
+wget http://step.esa.int/downloads/5.0/installers/esa-snap_sentinel_unix_5_0.sh  >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+SECONDS=0
+echo -ne " Installing the SNAP software ..." &&
+sh esa-snap_sentinel_unix_5_0.sh -q -overwrite  >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+rm -f esa-snap_sentinel_unix_5_0.sh
+
+echo -ne " Adding environmental variables to /etc/environment ..."
+echo 'SNAP_EXE=/usr/local/snap/bin/gpt' | tee -a /etc/environment
+
+# update SNAP
+SECONDS=0
+echo -ne " Updating SNAP to the latest version ..." &&
+#/usr/local/snap/bin/snap
+snap --nosplash --nogui --modules --update-all  >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+HOME_USER=`stat -c '%U' ${HOME}/.bashrc`
+chown -R ${HOME_USER}:${HOME_USER} ${HOME}/.snap
+
+#-------------------------------------
+
+#------------------------------------------------------------------
+# 5 Download the additional Database
+#------------------------------------------------------------------
+
+echo -ne " Installing R packages for shiny app ..." &&
+/usr/bin/R -e "install.packages('shiny', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('shinydashboard', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('shinyFiles', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('shinyjs', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('RSQLite', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('RColorBrewer', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('random', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('raster', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('mapview', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+/usr/bin/R -e "install.packages('rknn', dependencies=TRUE, repos='http://cran.rstudio.com/')" >> ${OSK_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo " Installation of OST dependencies succesfully completed"
+echo "---------------------------------------------------------------------------------------------------------------------------"
---
+++ bins/Install_OST/ost_installer.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+# Copyright (c) 2016 Andreas Vollrath
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is furnished
+# to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+
+OST_VERSION="0.1-alpha.1"
+
+if [ "$#" == "2" ];then
+
+	echo -e ""
+	echo -e "----------------------------------"
+	echo -e " Open SAR Toolkit, version ${OSK_VERSION}"
+	echo -e " Install script"
+	echo -e "----------------------------------"
+	echo -e ""
+
+	export OST_HOME=$(readlink -f $1)
+
+else
+
+	echo -e ""
+	echo -e "----------------------------------"
+	echo -e " Open SAR Toolkit, version ${OSK_VERSION}"
+	echo -e " Install script"
+	echo -e "----------------------------------"
+	echo -e ""
+
+	echo -e " Usage: install_ost <install dir> <dependencies> <output resolution>" # <elevation> "
+	echo -e ""
+	echo -e " input parameters:"
+	echo -e "  install dir		(input) path to OST installation"
+	echo -e "  dependencies: "
+	echo -e "			  Available choices:"
+	echo -e "			  0: install without dependencies "
+	echo -e "			  1: install dependencies and set up environmental variables"
+	echo -e "			  "
+	echo -e ""
+	exit 1
+
+fi
+
+mkdir -p ${OST_HOME}
+mkdir -p ${OST_HOME}/LOG
+OPENSARKIT=${OST_HOME}
+
+# get OST tagged version from github
+SECONDS=0
+echo -ne " Getting the Open SAR Toolkit ..." &&
+wget https://github.com/openforis/opensarkit/archive/v0.1-alpha.1.tar.gz
+tar -xzf v0.1-alpha.1.tar.gz
+mv opensarkit-0.1-alpha.1/* ${OST_HOME}
+rm -rf opensarkit-0.1-alpha.1 v0.1-alpha.1.tar.gz
+echo "OPENSARKIT=${OST_HOME}" | tee -a /etc/environment
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+
+OST_VERSION=0.1-alpha.1
+
+cd ${OST_HOME}/bins
+
+BINDIR=/usr/local/bin/
+
+for OST_BINS in `ls -1 -d */`;do
+
+	cd $OST_BINS
+	for exe in `ls -1 {ost_*,post_*} 2>/dev/null`;do
+
+		exepath=`readlink -f $exe`
+		ln -s $exepath ${BINDIR}/
+
+	done
+
+	cd ../
+done
+
+#------------------------------------------------------------------
+# Download the OST Database
+#------------------------------------------------------------------
+
+mkdir -p ${OST_HOME}/Database
+cd ${OST_HOME}/Database
+
+SECONDS=0
+echo -ne " Downloading the OST database ..."
+echo "OST_DB=${OST_HOME}/Database/OST_db.sqlite" | tee -a /etc/environment
+wget https://www.dropbox.com/s/qvujm3l0ba0frch/OFST_db.sqlite?dl=0  >> ${OST_HOME}/LOG/log_install 2>&1
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+mv OFST_db.sqlite?dl=0 OST_db.sqlite
+
+#------------------------------------------------------------------
+# Create a start-up icon
+#------------------------------------------------------------------
+echo -ne " Creating a start up icon on the Desktop ..."
+echo "[Desktop Entry]" > ${HOME}/Desktop/OST.desktop
+echo "Version=0.1-alpha.1" >> ${HOME}/Desktop/OST.desktop
+echo "Name=Open SAR Toolkit" >> ${HOME}/Desktop/OST.desktop
+echo "Comment=" >> ${HOME}/Desktop/OST.desktop
+echo 'Exec=bash -c "R CMD BATCH ${OST_HOME}/shiny/ost.R;$SHELL"' >> ${HOME}/Desktop/OST.desktop
+echo "Icon=${OST_HOME}/OST_icon_trans.png" >> ${HOME}/Desktop/OST.desktop
+echo "Terminal=true" >> ${HOME}/Desktop/OST.desktop
+echo "Type=Application" >> ${HOME}/Desktop/OST.desktop
+echo "Categories=Application;" >> ${HOME}/Desktop/OST.desktop
+echo "Path=" >> ${HOME}/Desktop/OST.desktop
+echo "StartupNotify=false" >> ${HOME}/Desktop/OST.desktop
+
+echo "require(shiny)" > ${OST_HOME}/shiny/ost.R
+echo "runApp('${OPENSARKIT}/shiny/',launch.browser = T)" >> ${OST_HOME}/shiny/ost.R
+
+chmod +x ${HOME}/Desktop/OST.desktop
+chown ${HOME_USER}:${HOME_USER} ${HOME}/Desktop/OST.desktop
+
+cp ${HOME}/Desktop/OST.desktop /usr/share/applications/OST.desktop
+
+duration=$SECONDS && echo -e " done ($(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed)"
+
+if [[ $2 == 1 ]];then
+	bash $OPENSARKIT/bins/ost_dependencies_ubuntu1604.sh
+fi
+
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo " Installation of OST succesfully completed"
+echo " In order to be able to launch the scripts immediately on the command line, type: source /etc/environment "
+echo " Otherwise restart your computer"
+echo "---------------------------------------------------------------------------------------------------------------------------"
